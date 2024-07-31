e1a5b35c491e2335462bef155b541352b54a3a0c
Vadim Stepanov <vstepanov@natera.com>
2024-07-31 19:43:02 +0200

    adding env for aws

--- bin/aws-demo-project-ts.ts
+++ bin/aws-demo-project-ts.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsDemoProjectTsStack } from '../lib/aws-demo-project-ts-stack';
 
 const app = new cdk.App();
-new AwsDemoProjectTsStack(app, 'AwsDemoProjectTsStack', {
+new AwsDemoProjectTsStack(app, 'Aws-Demo-Stack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new AwsDemoProjectTsStack(app, 'AwsDemoProjectTsStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '381492166005', region: 'us-east-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
