ff566098e118f5a29a1c0b56d3604a5f5e9bd9c3
Vadim Stepanov <vstepanov@natera.com>
2024-07-31 20:40:14 +0200

    creating first lambda func

--- lib/aws-demo-project-ts-stack.ts
+++ lib/aws-demo-project-ts-stack.ts
@@ -1,16 +1,20 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import path = require('path');
 
 export class AwsDemoProjectTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsDemoProjectTsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new NodejsFunction(this, 'hello-world', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'handler',
+      entry: path.join(__dirname, '../src/functions/index.ts'),
+      environment: {},
+      functionName: `hello-world`,
+      architecture: lambda.Architecture.ARM_64,
+    });
   }
 }
