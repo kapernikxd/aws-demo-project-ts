a2a1bc540d4c5b5fb74b98cdef07179148522a01
Vadim Stepanov <vstepanov@natera.com>
2024-07-31 20:50:07 +0200

    creating dynamodb.Table

--- lib/aws-demo-project-ts-stack.ts
+++ lib/aws-demo-project-ts-stack.ts
@@ -2,11 +2,19 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import path = require('path');
 
 export class AwsDemoProjectTsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // creating DynamoDB table
+    new dynamodb.Table(this, 'AwsDemoProjectTable', {
+        partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+        tableName: "AwsDemoProjectTable", //table name,
+        removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
     new NodejsFunction(this, 'hello-world', {
       runtime: lambda.Runtime.NODEJS_16_X,
